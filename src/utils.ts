b25aeac1b5e6d4850e50e613af903bff555f0cca
Ivo Dolenc <ivodolenc@gmail.com>
2025-02-01 15:07:43 +0100

    refactor: update isBrowser util

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,6 @@
 import type { FrameState } from './types'
 
-export const isBrowser =
-  typeof window !== 'undefined' && typeof document !== 'undefined'
+export const isBrowser = typeof window !== 'undefined'
 
 export const defaultState = (): FrameState => ({
   delta: 0.0,
