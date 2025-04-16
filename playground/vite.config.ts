5e74f5b6b6d5bdae76ea5cd4393e2dcfb787261c
Ivo Dolenc <ivodolenc@gmail.com>
2025-04-16 15:20:18 +0200

    chore: update playground

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,11 +1,9 @@
 import { cwd } from 'node:process'
 import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   resolve: {
     alias: { '@': resolve(cwd(), './src') },
   },
-  plugins: [tailwindcss()],
 })
