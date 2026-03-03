f8e550e48de93ed4c951f81e2f00a4d26552e75b
Ivo Dolenc <ivodolenc@gmail.com>
2026-03-03 09:41:45 +0100

    chore: update playground

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -3,7 +3,11 @@ import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  server: { hmr: false },
   resolve: {
-    alias: { '@': resolve(cwd(), './src') },
+    alias: {
+      '@': resolve(cwd(), './src'),
+      '@/*': resolve(cwd(), './src'),
+    },
   },
 })
